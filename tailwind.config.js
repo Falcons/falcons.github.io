09d3a294d602fbaaf266f3ebcdf691d481d89784
eGuitar999 <ari.vancruyningen@gmail.com>
2024-08-05 21:51:27 -0400

    added testdoc

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         robot_bg: "linear-gradient(to top, rgba(60, 60, 60, 1), rgba(243, 244, 246, 0.5)), url(/assets/images/team/team-candid.jpg)",
         team_bg: "linear-gradient(to top, rgba(60, 60, 60, 1), rgba(243, 244, 246, 0.5)), url(/assets/images/team/team-with-doug.jpg)",
         frc_bg: "linear-gradient(to top, rgba(60, 60, 60, 1), rgba(243, 244, 246, 0.5)), url(/assets/images/frc/event.png)",
-        translucent: "linear-gradient(to top, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.5))"
+        translucent: "linear-gradient(to top, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.4))"
       }
     },
   },
